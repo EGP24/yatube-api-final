01c98c112ecba618266e6ce1c1d4036122b0927c
v.pereverza <v.pereverza@cian.ru>
2025-05-16 11:55:28 +0300

    added admin page

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -4,13 +4,31 @@ from .models import Comment, Follow, Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group', 'image')
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'
+    empty_value_display = '-нет-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title', 'slug')
+    empty_value_display = '-нет-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'author', 'post', 'created')
+    search_fields = ('text',)
+    list_filter = ('created',)
+    empty_value_display = '-нет-'
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'following')
+    empty_value_display = '-нет-'
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
-admin.site.register(Comment)
-admin.site.register(Follow)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
